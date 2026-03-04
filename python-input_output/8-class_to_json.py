60c2b0731d64fced1a3234589987c284c73397a3
iame1vn <ku24elvin@gmail.com>
2026-03-04 07:21:49 +0000

    neyse

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,6 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description of an object for JSON serialization"""
+    """Return the dictionary description of an object for JSON
+    serialization"""
     return obj.__dict__
