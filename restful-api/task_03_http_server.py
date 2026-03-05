d7d55850c10c8580d350c44fb060cb383a5c85ed
iame1vn <ku24elvin@gmail.com>
2026-03-05 07:07:08 +0000

    isle de

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,7 +15,13 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode("utf-8"))
-        elif self.path == "/status":  # test bunu yoxlayır
+        elif self.path == "/status":
+            status_data = {"status": "OK"}  # testin tələb etdiyi məzmun
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(status_data).encode("utf-8"))
+        elif self.path == "/info":
             info_data = {"version": "1.0", "description": "A simple API built with http.server"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
